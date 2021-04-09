50e65ba32f791e3f4a8b6599098b1da12d79f337
Oliver Nygren <oliver.nygren@icloud.com>
2021-04-09 12:54:17 +0200

    set up script file and html + css for client interface

---
+++ public/script.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', init)
+
+async function init() {
+  const seeAllProductsBtn = document.getElementById('get');
+  const addNewProductBtn = document.getElementById('post');
+  const updateProductBtn = document.getElementById('put');
+  const deleteProductBtn = document.getElementById('delete');
+
+  seeAllProductsBtn.addEventListener('click', () => {
+    console.log('get')
+  })
+
+  addNewProductBtn.addEventListener('click', () => {
+    console.log('post')
+  })
+
+  updateProductBtn.addEventListener('click', () => {
+    console.log('put')
+  })
+
+  deleteProductBtn.addEventListener('click', () => {
+    console.log('delete')
+  })
+
+  const allProducts = await getAllProducts()
+  const addedProduct = await addNewProduct("Yoghurt", 60, 14)
+  const updatedProduct = await updateProduct(149)
+  const removedProduct = await deleteProduct()
+}
+
+async function getAllProducts() {
+  const products = await makeRequest('/', 'GET')
+  return products
+}
+
+async function addNewProduct(name, calories, price) {
+
+  const body = {
+    name: name,
+    calories: calories,
+    price: price
+  }
+
+  const newProduct = await makeRequest('/', 'POST', body)
+  return newProduct
+}
+
+async function updateProduct(price) {
+  const updatedProduct = await makeRequest('/', 'PUT')
+}
+
+async function deleteProduct() {
+  const deletedProduct = await makeRequest('/', 'DELETE')
+}
+
+
+
+async function makeRequest(url, method, body) {
+  const response = await fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+
+  const result = await response.json()
+
+  return result
+}
--- server.js
+++ server.js
@@ -67,6 +67,7 @@ let products =
   }
 ]
 
+app.use(express.static('./public'))
 app.use(express.json());
 
 
